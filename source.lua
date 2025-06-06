c216d39dfa014c435fcdbf8688c9c4fab7d3f0c9
CryooOfficial <cryooofficial@gmail.com>
2025-06-06 12:49:01 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -686,7 +686,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=87713000038503",
+		Image = "http://www.roblox.com/asset/?id=83761173072355",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
